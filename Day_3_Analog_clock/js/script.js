df219d57760c6a46c828c616a3c4f1d8f6737795
Chithra Devi A.N <44505676+chithra98devi@users.noreply.github.com>
2023-04-16 19:37:37 +0530

    Add files via upload

---
+++ Day_3_Analog_clock/js/script.js
@@ -0,0 +1,20 @@
+setInterval(runClock, 1000);
+
+const hour = document.querySelector(".hrs");
+const minute = document.querySelector(".min");
+const second = document.querySelector(".sec");
+
+function runClock() {
+  const time = new Date();
+  const sec = time.getSeconds() / 60;
+  const min = (sec + time.getMinutes()) / 60;
+  const hrs = (min + time.getHours()) / 12;
+
+  hour.style.setProperty("--rotation", hrs * 360);
+  minute.style.setProperty("--rotation", min * 360);
+  second.style.setProperty("--rotation", sec * 360);
+
+  // console.log(hrs, min, sec);
+}
+
+runClock();
